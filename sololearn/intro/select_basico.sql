ecbdcaaf5691d648e507769bfefdf8d126ea3867
Elias Cruz Álvarez <eliascza@gmail.com>
2026-01-16 16:57:48 -0600

    Update and rename select_basico.sql to trabajando con datos.sql

--- sololearn/intro/select_basico.sql
+++
@@ -1,4 +0,0 @@
--- INSTRUCCIÓN: Completa la consulta para seleccionar los campos de name y price de la tabla de products.
-
-SELECT name, price
-FROM products;
---
+++ sololearn/intro/trabajando con datos.sql
@@ -0,0 +1,4 @@
+-- INSTRUCCIÓN: Completa la consulta para extraer todos los campos de la tabla flights
+
+SELECT title
+FROM movies
