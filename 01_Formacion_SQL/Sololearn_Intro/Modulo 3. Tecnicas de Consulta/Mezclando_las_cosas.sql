de5ee99af94f4f0add58831f682d90c47d8db644
Elias Cruz Álvarez <eliascza@gmail.com>
2026-01-26 06:09:11 -0600

    Create Mezclando_las_cosas.sql with aggregation example
    Added SQL query to calculate average price from sales.

---
+++ 01_Formacion_SQL/Sololearn_Intro/Modulo 3. Tecnicas de Consulta/Mezclando_las_cosas.sql
@@ -0,0 +1,6 @@
+/* Las operaciones de agregación generalmente se denominan funciones de agregación. Una función incluye el código necesario para realizar una tarea.*/
+
+-- INSTRUCCIÓN: Completa la consulta utilizando la función de agregación
+
+SELECT AVG(price)
+FROM sales;
